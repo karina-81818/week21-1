97398859877ae915e1454c1102155872562ca22e
Karina <karina.isl1806@gmail.com>
2023-11-03 15:46:37 +0900

    addscript

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { Chart } from "react-google-charts";
 
-function App() {
+export const data = [
+  ["Task", "Hours per Day"],
+  ["Работа", 6],
+  ["Спорт", 1],
+  ["Учеба", 6],
+  ["Дети", 12],
+  ["Сон", 6],
+];
+
+export const options = {
+  title: "Распределение дня",
+};
+
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"400px"}
+    />
   );
 }
+
+
 
 export default App;
